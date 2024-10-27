5be0c7488332aa3d9b2ead863b811b7a5df973b5
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2024-10-28 00:36:38 +0900

    feat: 문제6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const set = new Set();
+  const map = new Map();
+
+  forms.forEach(([email, nickname]) => {
+    for (let i=0; i<nickname.length-1; ++i) {
+      for (let j=i+2; j<=nickname.length; ++j) {
+        const substr = nickname.slice(i, j);
+        if (map.has(substr)) {
+          set.add(email);
+          set.add(map.get(substr));
+        }
+        else map.set(substr, email);
+      }
+    }
+  });
+
+  return Array.from(set).sort();
 }
 
 module.exports = problem6;
