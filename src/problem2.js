b7adc3b52584498b4ea51bdf3babb54b7e2e96cb
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2024-10-27 22:25:29 +0900

    feat: 문제2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [];
+
+  [...cryptogram].forEach(char => {
+    if(stack.at(-1) != char) stack.push(char);
+    else stack.pop();
+  });
+  
+  return stack.toString().replaceAll(',','');
 }
 
 module.exports = problem2;
