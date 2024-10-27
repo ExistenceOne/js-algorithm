e39b94218e9a59ecb4ae90f3207dc7439b9d0684
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2024-10-27 23:18:25 +0900

    feat: 문제3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  for (let i=1; i<=number; ++i) count += count369(i);
+  return count;
+}
+
+function count369(number){
+  let count = 0;
+  while (number > 0){
+    switch (number % 10) {case 3: case 6: case 9: ++count};
+    number = parseInt(number / 10);
+  }
+  return count;
 }
 
 module.exports = problem3;
