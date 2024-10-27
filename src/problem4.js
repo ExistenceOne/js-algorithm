84a329ab08ea8fb3c7dcb11875313660972d4b99
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2024-10-28 00:04:56 +0900

    feat: 문제4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,15 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+
+  [...word].forEach(char => {
+    if (/[A-Z]/.test(char))
+      answer += String.fromCharCode(65+25-(char.charCodeAt()-65));
+    else if (/[a-z]/.test(char))
+      answer += String.fromCharCode(97+25-(char.charCodeAt()-97));
+    else
+      answer += char;
+  });
+  
   return answer;
 }
 
