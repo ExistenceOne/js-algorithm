a9995580bc7c24f1646a1883ece33da2ad6bec0b
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2024-10-28 00:16:22 +0900

    feat: 문제5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  const answer = [];
+  const denoms = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  denoms.forEach(denom => {
+    answer.push(parseInt(money / denom));
+    money %= denom;
+  });
   return answer;
 }
 
